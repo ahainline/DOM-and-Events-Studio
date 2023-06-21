98ee0d42b933a86c47fc34ce5d0173604b8b47cc
Ashley Hainline <aetindle@gmail.com>
2023-06-21 11:06:24 -0500

    takeoff button functionality

--- scripts.js
+++ scripts.js
@@ -1,2 +1,16 @@
 // Write your JavaScript code here.
 // Remember to pay attention to page loading!
+window.addEventListener('load', function() {
+    const takeOffButton = document.getElementById('takeoff');
+    const flightStatus = document.getElementById('flightStatus');
+    const shuttleBackground = document.getElementById('shuttleBackground');
+    const shuttleHeight = document.getElementById('spaceShuttleHeight');
+    takeOffButton.addEventListener('click', () => {
+        const takeOffConfirmed = window.confirm('Confirm that the shuttle is ready for takeoff.');
+        if (takeOffConfirmed === true) {
+            flightStatus.innerHTML = 'Shuttle in flight.';
+            shuttleBackground.style.backgroundColor = 'blue';
+            shuttleHeight.innerHTML = (Number(shuttleHeight.innerHTML) + 10000);
+        }
+    });
+});
